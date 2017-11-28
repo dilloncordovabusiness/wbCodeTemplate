9d091145bd4d5ea15fc6534a37b91574a532b51a
dilloncordovabusiness <34075454+dilloncordovabusiness@users.noreply.github.com>
2017-11-28 14:07:45 -0800

    Add files via upload

---
+++ unit.spec.js
@@ -0,0 +1,60 @@
+const FRAPPLESS = require('frappless-mocha');
+const INDEX     = require('../index');
+
+describe('describe', () => {
+
+    /**
+     Specs Start
+     */
+    let event, errorObj, dynamoResponse, requestResponse;
+
+    beforeEach( () => {
+        event           = {};
+        errorObj        = {};
+        dynamoResponse  = {};
+        requestResponse = {};
+
+        FRAPPLESS.nock('https://www.google.com')
+            .post   ('')
+            .reply  ((_response) => {
+                return requestResponse;
+            })
+        ;
+
+        FRAPPLESS.aws( 'DynamoDB', 'getItem', function (params, callback) {
+            callback(errorObj, dynamoResponse);
+        });
+    });
+
+    afterEach( () => {
+        FRAPPLESS.clean();
+    });
+    /**
+     Specs End
+     */
+
+    /**
+     Tests Start
+     */
+    context( 'context', () => {
+
+        it.EtoE('should', ( _done ) => {
+            INDEX.handler(event, {}, function( __err, __data ){
+                expect(false).to.be.equal(true);
+                _done();
+            })
+        });
+
+        it('should', ( _done ) => {
+            INDEX.handler(event, {}, function( __err, __data ){
+                expect(false).to.be.equal(true);
+                _done();
+            })
+        });
+
+    });
+    /**
+     Tests End
+     */
+
+});
